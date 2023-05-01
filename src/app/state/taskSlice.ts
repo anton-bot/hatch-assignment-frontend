1c6bf90c956c01c62474cb5cae2386f022f25496
Anton Ivanov <anton@ivanov.hk>
2023-05-01 01:20:52 -0700

    feat: connect to remaining endpoints: search, done

--- src/app/state/taskSlice.ts
+++ src/app/state/taskSlice.ts
@@ -2,11 +2,14 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { Task } from '../../types/Task';
 import { fetchAllTasks } from '../../api/fetchAllTasks';
 import { createNewTask } from '../../api/createNewTask';
+import { deleteAllTasks } from '../../api/deleteAllTasks';
+import { markCompleted } from '../../api/markCompleted';
 
 export type TaskState = {
   active: Task[];
   done: Task[];
   newTaskLabel: string;
+  filter: string;
   status: 'idle' | 'loading' | 'failed';
 };
 
@@ -14,10 +17,14 @@ const initialState: TaskState = {
   active: [],
   done: [],
   newTaskLabel: '',
+  filter: '',
   status: 'idle',
 };
 
-export const getTasksAsync = createAsyncThunk('task/getTasks', async () => fetchAllTasks());
+export const getTasksAsync = createAsyncThunk(
+  'task/getTasks',
+  async (filter?: string) => await fetchAllTasks(filter),
+);
 export const createNewTaskAsync = createAsyncThunk(
   'task/createNewTask',
   async (label: string, { dispatch }) => {
@@ -26,6 +33,21 @@ export const createNewTaskAsync = createAsyncThunk(
     }
 
     await createNewTask(label);
+    dispatch(setNewTaskLabel(''));
+    dispatch(getTasksAsync());
+  },
+);
+export const deleteAllTasksAsync = createAsyncThunk(
+  'task/deleteAllTasks',
+  async (_, { dispatch }) => {
+    await deleteAllTasks();
+    dispatch(getTasksAsync());
+  },
+);
+export const markCompletedAsync = createAsyncThunk<void, { id: string; done: boolean }>(
+  'task/markCompleted',
+  async ({ id, done }, { dispatch }) => {
+    await markCompleted(id, done);
     dispatch(getTasksAsync());
   },
 );
@@ -36,6 +58,9 @@ export const counterSlice = createSlice({
   reducers: {
     setNewTaskLabel: (state, action) => {
       state.newTaskLabel = action.payload;
+    },
+    setFilter: (state, action) => {
+      state.filter = action.payload;
     },
   },
   extraReducers: (builder) =>
@@ -59,13 +84,23 @@ export const counterSlice = createSlice({
       })
       .addCase(createNewTaskAsync.rejected, (state) => {
         state.status = 'failed';
+      })
+      .addCase(deleteAllTasksAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(deleteAllTasksAsync.fulfilled, (state) => {
+        state.status = 'idle';
+      })
+      .addCase(deleteAllTasksAsync.rejected, (state) => {
+        state.status = 'failed';
       }),
 });
 
-export const { setNewTaskLabel } = counterSlice.actions;
+export const { setNewTaskLabel, setFilter } = counterSlice.actions;
 
 export const selectActiveTasks = (state: { task: TaskState }) => state.task.active;
 export const selectDoneTasks = (state: { task: TaskState }) => state.task.done;
 export const selectNewTaskLabel = (state: { task: TaskState }) => state.task.newTaskLabel;
+export const selectFilter = (state: { task: TaskState }) => state.task.filter;
 
 export default counterSlice.reducer;
