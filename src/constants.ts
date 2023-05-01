f8f5c5b09223497bdbfcdcf893bdfd5fa883e3b0
Anton Ivanov <anton@ivanov.hk>
2023-05-01 03:47:29 -0700

    fix: add real production api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 const LOCAL_URL = 'http://localhost:3001/api';
-const PROD_URL = 'TODO: add production url';
+const PROD_URL = 'https://hatch-assignment-backend.azurewebsites.net/api';
 
 export const API_URL = process.env.ENVIRONMENT === 'production' ? PROD_URL : LOCAL_URL;
